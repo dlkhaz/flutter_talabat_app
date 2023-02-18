f460d55c937558c4afe0b1f32231630cf07f665a
dlkhaz <dlkaz.jh7720@stu.uod.ac>
2023-02-18 20:37:19 +0300

    ddd

--- lib/Dlkhaz/Dscreen1.dart
+++ lib/Dlkhaz/Dscreen1.dart
@@ -8,7 +8,7 @@ class Dleclass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar( 
-      backgroundColor: Color.fromARGB(255, 147, 184, 63), 
+      backgroundColor: Color.fromARGB(255, 102, 73, 139), 
       title: Center(child: Text("dlxaz Screen")),),);
 }
 } 
