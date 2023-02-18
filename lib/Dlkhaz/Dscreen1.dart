7c30b89487172f377ad1dc9627c1ab76aecfab3b
dlkhaz <dlkaz.jh7720@stu.uod.ac>
2023-02-18 19:50:28 +0300

    Dlkhaz Screen

--- lib/Dlkhaz/Dscreen1.dart
+++ lib/Dlkhaz/Dscreen1.dart
@@ -1,16 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class Dleclass extends StatefulWidget {
+  const Dleclass({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<Dleclass> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyWidgetState extends State<Dleclass> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: Center(child: Text("My Screen")),
+    backgroundColor:Color.fromARGB(255, 104, 8, 8)),);
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_talabat_app/Dlkhaz/Dscreen1.dart';
 import 'package:flutter_talabat_app/saraa%20folder/sarascreen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Saraclas(),
+      home: Dleclass(),
     );
   }
 }
