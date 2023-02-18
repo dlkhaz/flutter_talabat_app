e1c66fdd86e7c4277fcf76832440df0a8506bcd6
dlkhaz <dlkaz.jh7720@stu.uod.ac>
2023-02-18 19:52:56 +0300

    DD

--- lib/Dlkhaz/Dscreen1.dart
+++ lib/Dlkhaz/Dscreen1.dart
@@ -2,18 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class Dleclass extends StatefulWidget {
+class Dleclass extends StatelessWidget {
   const Dleclass({super.key});
 
   @override
-  State<Dleclass> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<Dleclass> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Center(child: Text("My Screen")),
-    backgroundColor:Color.fromARGB(255, 104, 8, 8)),);
-  }
+    backgroundColor: Color.fromARGB(255, 138, 65, 93),),);
 }
-
+}
